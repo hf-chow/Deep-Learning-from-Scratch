092a559f5c9e790e8b26ee5ce19a40e38de3a60d
Hon Chow <honfungchow@gmail.com>
2024-04-18 01:17:36 +0800

    added sigmoid and sigmoid_deriv

--- dl_from_scratch/model/mlp.py
+++ dl_from_scratch/model/mlp.py
@@ -2,3 +2,17 @@ from utils import dataloader
 
 import numpy as np
 
+def sigmoid(x: np.float64) -> np.float64:
+    return 1 / (1+np.exp(-x))
+
+def sigmoid_deriv(x: np.float64) -> np.float64:
+    return sigmoid(x) * (1-sigmoid(x))
+
+def forward(input: np.ndarray, weight: np.ndarray) -> np.float64:
+    # Forward pass is a matrix vector multiplication between the input vector
+    # and the weight matrix. For MNIST we will flatten the entire 20x20 array
+    # into a 1D vector and the resultant will be reduced a single float
+    results = np.matmul(input, weight)
+    results = sigmoid(results)
+    return  results
+    
