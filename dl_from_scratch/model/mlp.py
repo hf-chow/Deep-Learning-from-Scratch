cd9b77fde0cf2a0e6796a36d9a4a9df2518cbb17
Hon Chow <honfungchow@gmail.com>
2024-05-12 01:12:17 +0800

    logsumpexp and relu implementation

--- dl_from_scratch/model/mlp.py
+++ dl_from_scratch/model/mlp.py
@@ -6,20 +6,7 @@ from typing import Callable
 from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-# TODO
-# - Forward Pass
-# - SGD
-# - Backprop
-# - Loss calculation
-# - Training loop
-# - Evaluation
-# - Plot
-
-# TO FIX
-# - Better names for params and functions
-# - OOP
+import pandas as pd
 
 def acc_loss_plot(accuracies, losses):
     x = list(range(1,len(accuracies)+1))
@@ -28,7 +15,8 @@ def acc_loss_plot(accuracies, losses):
     ax1.plot(x, accuracies,label="Accuracy")
 
     ax2 = ax1.twinx()
-    ax2.plot(x, losses,label="Loss")
+    ax2.plot(x, losses,label="Loss", color="orange")
+
     plt.savefig("plot.png")
 
 def logsumexp(x):
@@ -40,6 +28,14 @@ def sigmoid(x):
 
 def d_sigmoid(x):
     return (np.exp(-x))/((np.exp(-x)+1)**2)
+
+def relu(x):
+    return np.maximum(x, 0)
+
+def d_relu(x):
+    x[x <= 0] = 0
+    x[x > 0] = 1
+    return x
 
 def map_labels(label):
     result = np.zeros((10, 1))
@@ -82,32 +78,56 @@ def SGD(l1, l2, d_l1, d_l2, lr=1e-3):
     l2 = l2 - lr*d_l2
     return l1, l2
 
+#TODO Refactor training loop to cater for different activation functions
 def forward_backward(x, y, l1, l2):
     # Forward  
     label = np.zeros((len(y), 10))
     label[range(label.shape[0]), y] = 1
     x_l1 = x.dot(l1)
-    l1_out = sigmoid(x_l1)
+
+    out = np.zeros((len(y), 10), np.float32)
+    out[range(out.shape[0]), y] = 1
+
+    # Sigmoid
+#    l1_out = sigmoid(x_l1)
+    # relu
+#    l1_out = relu(x_l1)
+    l1_out = np.maximum(x_l1, 0)
+
     x_l2 =  l1_out.dot(l2)
-    x_out = softmax(x_l2)
-
+#    x_out = softmax(x_l2)
+    x_lsm = x_l2 - logsumexp(x_l2).reshape((-1, 1))
+    x_loss = (-out * x_lsm).mean(axis=1)
+    
     # Backward
-    d_sm = d_softmax(x_l2)
-    x_out_error = 2*d_sm*(x_out-label)/x_out.shape[0]
-    d_l2 = l1_out.T.dot(x_out_error)
-    d_l2_error = ((l2).dot(x_out_error.T)).T*d_sigmoid(x_l1)
-    d_l1 = x.T.dot(d_l2_error)
+    d_out = -out/len(y)
+    dx_lsm = d_out - np.exp(x_lsm)*d_out.sum(axis=1).reshape((-1, 1))
+#    d_sm = d_softmax(x_l2)
+#    x_out_error = 2*d_sm*(x_out-label)/x_out.shape[0]
+#    d_l2 = l1_out.T.dot(x_out_error)
+    d_l2 = l1_out.T.dot(dx_lsm)
+    dx_relu = dx_lsm.dot(l2.T)
 
-    return x_out, d_l1, d_l2
+#    d_l2_error = ((l2).dot(x_out_error.T)).T*d_sigmoid(x_l1)
+#    d_l2_error = ((l2).dot(x_out_error.T)).T*d_relu(x_l1)
+    dx_l1 = (l1_out > 0).astype(np.float32)*dx_relu
+    d_l1 = x.T.dot(dx_l1)
+
+    return x_l2, x_loss, d_l1, d_l2
 
 def train():
-    PATH = "../../data/MNIST/test.hdf5"
+    PATH = "../../data/MNIST/train.hdf5"
     x_train, y_train = load_hdf5(PATH)
+#    TRAIN_PATH = "../../data/MNIST/train.csv"
+#    y_train = pd.read_csv(TRAIN_PATH)["label"]
+#    x_train = pd.read_csv(TRAIN_PATH).drop(labels=["label"], axis=1)
+
+#    print(x_train.head())
     
     l1 = init_layer(784, 128, mode="uniform")
     l2 = init_layer(128, 10, mode="uniform")
 
-    epochs = 10000
+    epochs = 1000
     batch_size = 128
 
     losses = []
@@ -117,12 +137,12 @@ def train():
         sample = np.random.randint(0, x_train.shape[0], size=(batch_size))
         x = x_train[sample].reshape((-1, 28*28))
         y = y_train[sample]
-        x_out , d_l1, d_l2 = forward_backward(x, y, l1, l2)
+        x_l2, x_loss, d_l1, d_l2 = forward_backward(x, y, l1, l2)
         l1, l2 = SGD(l1, l2, d_l1, d_l2)
 
-        pred = np.argmax(x_out,axis=1)
+        pred = np.argmax(x_l2,axis=1)
         acc = (pred == y).mean()
-        loss = cross_entropy_loss(x_out, y)
+        loss = x_loss.mean()
 
         losses.append(loss)
         accuracies.append(acc)
